3c842049a5784fd43b8d9c4e0c957fa93a3eccfd
Harsh <harshradadiya9999@gmail.com>
2024-04-26 17:21:06 +0530

    FIRST COMMIT

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import Image from "next/image";
 import { Card } from "@repo/ui/card";
 import { Code } from "@repo/ui/code";
 import styles from "./page.module.css";
 import { Button } from "@repo/ui/button";
+import { add } from "shared-utils";
 
 function Gradient({
   conic,
@@ -77,6 +80,8 @@ export default function Page(): JSX.Element {
           </a>
         </div>
       </div>
+
+      <button onClick={() => alert(add(1, 2))}>Add button</button>
 
       <Button appName="web" className={styles.button}>
         Click me!
